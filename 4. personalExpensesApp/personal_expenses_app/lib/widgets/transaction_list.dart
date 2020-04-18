970bb9e040ada869ea855ef15f03665da7541e0b
enamulhaque <enamulhaque028@gmail.com>
2020-04-18 22:10:07 +0600

    [Sec04]Configuring and Using Themes upto lec-30

--- 4. personalExpensesApp/personal_expenses_app/lib/widgets/transaction_list.dart
+++ 4. personalExpensesApp/personal_expenses_app/lib/widgets/transaction_list.dart
@@ -21,7 +21,7 @@ class TransactionList extends StatelessWidget {
                   ),
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                       width: 2,
                     ),
                   ),
@@ -31,7 +31,7 @@ class TransactionList extends StatelessWidget {
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
